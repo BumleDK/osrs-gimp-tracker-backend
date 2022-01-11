378180e8acff3361207a51d5bb810bfaf17779be
BumleDK <65760187+BumleDK@users.noreply.github.com>
2022-01-11 05:19:37 +0100

    update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 5000;
-const PASSWORD = process.env.GIMPTRACKER_PASSWORD || "ggbanden";
+const PASSWORD = process.env.GIMPTRACKER_PASSWORD || "password123";
 
 module.exports = 
 {
